188fa8557885ae4fde3ea0b1424cbd32e339b943
Pabricio Freitas <pabricio2000@gmail.com>
2026-08-06 14:22:24 -0300

    teste

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ origins = [o for o in [os.getenv("FRONTEND_URL")] if o]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
